0824c5aa01d7cd4c52652e200ab06dd7772fa546
chris.j.carter <chris.j.carter91@gmail.com>
2019-06-21 11:35:52 -0600

    Functionality and questions answered

--- Answers.md
+++ Answers.md
@@ -1,6 +1,25 @@
 1.  Name 3 JavaScript Array/Object Methods that do not produce side-effects? Which method do we use to create a new object while extending the properties of another object?
+
+map, filter, and concat
+
 1.  Describe `actions`, `reducers` and the `store` and their role in Redux. What does each piece do? Why is the store known as a 'single source of truth' in a redux application?
+
+actions respond to data moving around in the application
+reducers specify how an application's state changes in response to actions
+store holds the state tree of an application and can be changed by dispatching an action to it. The store is known as the single source of truth because it holds the entire state tree and can be referred back to after changes.
+
 1.  What is the difference between Application state and Component state? When would be a good time to use one over the other?
+
+Application state is more broadly used across the entire app where as component state is used in a certain component such as inputs on a form. 
+
 1.  What is middleware?
+
+Middleware adds a point between dispatching an action and when it reaches a reducer. Used for logging in/authentication.
+
 1.  Describe `redux-thunk`, what does it allow us to do? How does it change our `action-creators`?
+
+Allows asyncronus to show a loading state while the api call is processing and before information is returned. Adds a "thinking" state (maybe not the best word)
+
 1.  Which `react-redux` method links up our `components` with our `redux store`?
+
+connect
--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchSmurfsStart } from '../actions/index';
+import { addSmurf } from '../actions/index';
+
+
 import './App.css';
 /*
  to wire this component up you're going to need a few things.
@@ -6,17 +11,89 @@ import './App.css';
  Just remember, `how do I `connect` my components to redux?`
  `How do I ensure that my component links the state to props?`
  */
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
-    );
+state={
+  smurf: {
+    name: '',
+    age: '',
+    height: '',
   }
 }
 
-export default App;
+componentDidMount(){
+  this.props.fetchSmurfsStart()
+}
+
+handleChange = event => {
+  event.preventDefault()
+  this.setState({
+    smurf: {
+      ...this.state.smurf,
+      [event.target.name]: event.target.value,
+    }
+  })
+}
+
+addSmurf = event => {
+  this.props.addSmurf(this.state.smurf)
+  this.setState({
+    smurf: {
+      name: '',
+      age: '',
+      height: '',
+    }
+  })
+}
+
+render() {
+  return (
+    <div className="App">
+      <h1>Redux Smurf Village</h1>
+        {this.props.smurfs.map((x, i) => (
+          <div  className="smurfs" key={x.name}>
+            <h4>{x.name}</h4>
+            <p>{x.age} smurf years</p>
+            <p>{x.height}</p>
+          </div>
+        ))}
+          <form onSubmit={this.addSmurf}>
+            <input 
+            type="string"
+            name="name"
+            value={this.state.smurf.name}
+            placeholder="Name"
+            onChange={this.handleChange}
+            />
+            <input 
+            type="number"
+            name="age"
+            value={this.state.smurf.age}
+            placeholder="Age"
+            onChange={this.handleChange}
+            />
+            <input 
+            type="string"
+            name="height"
+            value={this.state.smurf.height}
+            placeholder="Height"
+            onChange={this.handleChange}
+            />
+            <button>Add a Smurf</button>
+          </form>
+    </div>
+  );
+}
+}
+
+const mapStateToProps = state => ({
+smurfs: state.smurfs
+})
+
+export default connect(
+mapStateToProps,
+{
+  fetchSmurfsStart,
+  addSmurf,
+}
+)(App);
